c00e84281e46ffe8b1e41edb04debb74d344a524
Qaid Jacobs <projects.qaid@gmail.com>
2016-06-16 12:25:05 +0200

    First commit of project contents

---
+++ SwiftBasics.playground/Pages/Types.xcplaygroundpage/Contents.swift
@@ -0,0 +1,35 @@
+//: [Previous](@previous)
+
+/////////////////////////
+/////// Strings /////////
+/////////////////////////
+
+// Concatenation
+
+let country = "Netherlands"
+let state = "North Holland"
+let city = "Amsterdam"
+let street = "Somestreet"
+
+let concatenatedAddress = country + ", " + state + ", " + city
+
+// String Interpolation
+
+let interpolatedAddress = "\(country) , \(state), \(city)"
+let interpolatedStree = "\("111") \(street)"
+
+
+/////////////////////////
+/////// Integers ////////
+/////////////////////////
+
+let favoriteProgrammingLanguage = "Swift"
+let year = 2014
+
+///////////////////////////////////////
+/////// Floating Point Numbers ////////
+///////////////////////////////////////
+
+var version = 2.0
+
+//: [Next](@next)
