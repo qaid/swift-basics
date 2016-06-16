4d6bd4cb51042d422524f46f8130fe435f13c699
Qaid Jacobs <projects.qaid@gmail.com>
2016-06-16 23:31:38 +0200

    Add binary operators with small expression

---
+++ SwiftBasics.playground/Pages/Operators.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//: [Previous](@previous)
+
+// Binary Operators
+
+let height = 10.2 // in feet
+let width = 12.3 // in feet
+
+let area = height * width
+
+// 1 sq meter = 1 sq. ft / 10.764
+let areaInSqaureMeters = area / 10.764
+
+//: [Next](@next)
