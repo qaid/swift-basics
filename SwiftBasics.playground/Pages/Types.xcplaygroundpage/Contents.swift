e03d9274e0be393f5fd519bdb00c6e8b6971c5a5
Qaid Jacobs <projects.qaid@gmail.com>
2016-06-16 15:12:04 +0200

    Added Type examples

--- SwiftBasics.playground/Pages/Types.xcplaygroundpage/Contents.swift
+++ SwiftBasics.playground/Pages/Types.xcplaygroundpage/Contents.swift
@@ -24,12 +24,41 @@ let interpolatedStree = "\("111") \(street)"
 /////////////////////////
 
 let favoriteProgrammingLanguage = "Swift"
-let year = 2014
+let year = 2014 // Int
 
 ///////////////////////////////////////
 /////// Floating Point Numbers ////////
 ///////////////////////////////////////
 
-var version = 2.0
+var version = 2.0 // Double
+
+/////////////////////////
+/////// Boolean /////////
+/////////////////////////
+
+let isAwesome = true // Bool
+
+
+/////////////////////////
+/////// Type Safety /////
+/////////////////////////
+
+var someString = ""
+
+let bestPlayer = "Michael Jordan"
+let averagePointsPerGame: Double = 30.1
+let yearOfRetirement: Int = 2003
+let hallOfFame: Bool = true
+
+//let firstValue: Int = 2
+//let secondValue: Int = 5
+//let product = firstValue * secondValue
+//let opening: String = "The product of"
+//let output: String = "\(opening) \(firstValue) \("times") \(secondValue) \("is") \(product)"
+
+let firstValue = 1
+let secondValue = 2
+let product = firstValue * secondValue
+let output = "\("The product of") \(firstValue) \("times") \(secondValue)  \("is") \(product)"
 
 //: [Next](@next)
