7aa2c5b38f3674158e303a7b1645239246bd93c8
Qaid Jacobs <projects.qaid@gmail.com>
2016-06-17 00:02:31 +0200

    List a few comparisons

--- SwiftBasics.playground/Pages/Operators.xcplaygroundpage/Contents.swift
+++ SwiftBasics.playground/Pages/Operators.xcplaygroundpage/Contents.swift
@@ -10,4 +10,23 @@ let area = height * width
 // 1 sq meter = 1 sq. ft / 10.764
 let areaInSqaureMeters = area / 10.764
 
+
+// Comparisons
+let value = 200
+let divisor = 5
+
+let someOperation = 20 + 400 % 10 / 2 - 15
+let anotherOperation = 52 * 27 % 200 / 2 + 5
+
+// Task 1 - Enter your code below
+let result = value % divisor
+
+// Task 2 - Enter your code below
+let isPerfectMultiple = result == 0
+
+// Task 3 - evaluate for value
+let isGreater = someOperation >= anotherOperation
+
+
+
 //: [Next](@next)
